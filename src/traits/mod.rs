4062cd9981a3ad51c673ffe8a74b52bb25b7c1dc
Aria Bagheri <ariab9342@gmail.com>
2025-02-21 16:52:42 +0000

    re-exported traits as pub

---
+++ src/traits/mod.rs
@@ -0,0 +1,15 @@
+mod causes;
+mod code;
+mod description;
+mod docs;
+mod from_code;
+mod message;
+mod solutions;
+
+pub use causes::*;
+pub use code::*;
+pub use description::*;
+pub use docs::*;
+pub use from_code::*;
+pub use message::*;
+pub use solutions::*;
